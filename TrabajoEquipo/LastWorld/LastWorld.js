f4606859b4e573a64e427c501a2994de7b536ac0
Fernando Núñez <126068178+FernandoNnez@users.noreply.github.com>
2024-10-25 19:47:32 -0600

    Trabajos en Equipo
    Tareas y/o actividades trabajadas en equipo

---
+++ TrabajoEquipo/LastWorld/LastWorld.js
@@ -0,0 +1,21 @@
+//IN: STRING s
+//OUT: LAST WOLRD reverse
+
+function lastWrld(s, n) {
+    let copy = "";
+    let reverse = "";
+    for (let i = n - 1; i >= 0; i--) {
+        if (s[i] == " ") {
+            break;
+        }
+        copy += s[i];
+    }
+    for (let v = copy.length - 1; v >= 0; v--) {
+        reverse += copy[v];
+    }
+    return reverse;
+}
+
+
+let cadena = "por mi raza hablara el espiritu";
+console.log(lastWrd(cadena, cadena.length));
